5897093d7ddc8eb157d760f50eb61df0153e5c48
aarika2948 <jalanaarika@gmail.com>
2026-06-28 00:38:52 +0530

    g

---
+++ PYnative/Basic Exercises for Beginners/exercise_25.py
@@ -0,0 +1,6 @@
+#Write a program that takes a year as input and determines if it is a leap year.
+year=int(input("Enter the year="))
+if (year%4==0)or(year%400==0):
+    print(f"{year} is a leap year.")
+else:
+    print(f"{year} is not a leap year.")
---
+++ PYnative/Basic Exercises for Beginners/exercise_26.py
@@ -0,0 +1,6 @@
+x=input("Enter the first set of data=").split()
+y=input("Enter the second set of data=").split()
+x_1=list(x)
+y_1=list(y)
+final=[x_1+y_1]
+print(final)
